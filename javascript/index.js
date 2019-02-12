c816c498f79b04dae903e5605910e085b32e79e1
Erick Sieben <erick@sieben.pw>
2019-02-12 11:28:02 -0700

    moving authentication check to server side php script

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,54 @@
-document.getElementById("goBtn").onclick = function () {
+//document.getElementById("goBtn").onclick = function () {
+//	var input = document.getElementById("password").elements[0].value;
+//	if (input == "siebenhome"){
+//		location.href = "home/home.html";
+//	}
+//	else {
+//		location.href = "#";
+//	}
+//}
+
+//Browser Support Code
+function ajaxFunction(){
+	var ajaxRequest;
+	
+	try {
+		//Opera, Firefox Safari
+		ajaxRequest = new XMLHttpRequest();
+	} catch (e) {
+		
+		//Internet Explorer
+		try {
+			ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			
+			try {
+				ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {
+
+				//Something went wrong
+				alert("Unable to support browser");
+				return false;
+			}
+		}
+	}
+		
+	//direct received data to appropriate html elements
+	ajaxRequest.onreadystatechange = function() {
+
+		//readyState = 4 indicates completed request
+		if(ajaxRequest.readyState == 4) {
+			if(ajaxRequest.responseText == "1") {
+				location.href = "home/home.html";
+			}
+			else {
+				location.href = "#";
+			}
+		}
+	}
+
+	//call server script to authenticate
 	var input = document.getElementById("password").elements[0].value;
-	if (input == "siebenhome"){
-		location.href = "home/home.html";
-	}
-	else {
-		location.href = "#";
-	}
+	ajaxRequest.open("GET", "../php/authenticate.php" + input, true);
+	ajaxRequest.send(null);
 }
