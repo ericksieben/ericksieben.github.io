1101ed9f931c8ee68ca9dc99bf8fdc2ddbd2d5db
Erick Sieben <erick@sieben.pw>
2019-04-28 21:05:29 -0600

    fixed calendar embed

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -34,7 +34,7 @@
                 </div>
 
                 <div class="content">
-                        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=mceeq2me93oaq554mc019g57pftga4fc%40impo$
+                        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%EFD68B&amp;color=%235229A3&amp;src=skopovcrnebcec6kt9n7jqufns%40group.calendar.google.com&amp;style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
                 </div>
         </div>
         </div>
