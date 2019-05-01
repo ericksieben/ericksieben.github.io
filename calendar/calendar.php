952e6f8180d9c681d0e125acbbb0c9e6ca7caac5
Erick Sieben <erick@sieben.pw>
2019-04-30 23:30:49 -0600

    debugging calendar size issue

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -34,7 +34,7 @@
                 </div>
 
                 <div class="content">
-                        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=mceeq2me93oaq554mc019g57pftga4fc%40import.calendar.google.com&amp;color=%23333333&amp;src=nhl_6_Edmonton%2BOilers%23sports%40group.v.calendar.google.com&amp;color=%238D6F47&amp;src=en.canadian%23holiday%40group.v.calendar.google.com&amp;color=%2329527A&amp;src=skopovcrnebcec6kt9n7jqufns%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=America%2FEdmonton" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=mceeq2me93oaq554mc019g57pftga4fc%40import.calendar.google.com&amp;color=%23333333&amp;src=nhl_6_Edmonton%2BOilers%23sports%40group.v.calendar.google.com&amp;color=%238D6F47&amp;src=en.canadian%23holiday%40group.v.calendar.google.com&amp;color=%2329527A&amp;src=skopovcrnebcec6kt9n7jqufns%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=America%2FEdmonton" style="border-width:0" width="99%" height="99%" frameborder="0" scrolling="no"></iframe>
                 </div>
         </div>
         </div>
